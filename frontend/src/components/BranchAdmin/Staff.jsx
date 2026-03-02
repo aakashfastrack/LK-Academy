47e62ff0b9d034034258a49cf04aa55b0b1662d4
navneetshahi14 <navneet.shahi2004@gmail.com>
2026-03-02 14:28:41 +0530

    updated branch admin

--- frontend/src/components/BranchAdmin/Staff.jsx
+++ frontend/src/components/BranchAdmin/Staff.jsx
@@ -90,7 +90,7 @@ const Staff = () => {
 
   const formatTime = (isoTime) => {
     if (!isoTime) return "";
-    const date = new Date(isoTime.replace("Z", ""));
+    const date = new Date(isoTime);
     return date.toLocaleTimeString("en-IN", {
       hour: "numeric",
       minute: "2-digit",
